5c1c1178231e3d78141a4b1d1fe7df0d5b3ad4a0
Eduardo Salinas <eduardo.salinas12@hotmail.com>
2025-01-18 22:22:13 -0600

    gh-pages configruation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/LasTlayuasFront",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
